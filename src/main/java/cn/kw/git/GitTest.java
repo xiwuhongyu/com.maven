33d3098d7cb1fa6e5df9c1e45bb5fca26af0ade3
xiwuhongyu <83845978+xiwuhongyu@users.noreply.github.com>
2022-09-14 22:21:48 +0800

    pull commit

--- src/main/java/cn/kw/git/GitTest.java
+++ src/main/java/cn/kw/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git hot-fix test ");
         System.out.println("hello git push test ");
         System.out.println("hello git push1 test ");
+        System.out.println("hello git pull test ");
 
     }
 }
