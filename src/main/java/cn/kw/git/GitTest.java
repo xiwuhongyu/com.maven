1428201996a3a67e2cdf7888d39a76fba2ab7b49
limin <liminxiwu@163.com>
2022-09-14 21:42:24 +0800

    push commit

--- src/main/java/cn/kw/git/GitTest.java
+++ src/main/java/cn/kw/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello git hot-fix ");
         System.out.println("hello git master test ");
         System.out.println("hello git hot-fix test ");
+        System.out.println("hello git push test ");
+
     }
 }
