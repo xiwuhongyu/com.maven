0b7c0bf27104fcf5b6adfb05fa403e0bf2611fa5
limin <liminxiwu@163.com>
2022-09-14 19:40:47 +0800

    hot commit

--- src/main/java/cn/kw/git/GitTest.java
+++ src/main/java/cn/kw/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git second ");
         System.out.println("hello git third");
+        System.out.println("hello git hot-fix ");
     }
 }
